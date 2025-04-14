064b107da952374e7036dd6ae04ff073cc52b4a1
Alexander Komarov <komarov@medvr.pro>
2025-04-14 11:46:16 +0300

    PuzzleSet scriptable object

---
+++ Assets/WordPuzzle/Runtime/PuzzleSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Codeabuse;
+using UnityEngine;
+
+namespace WordPuzzle
+{
+    [CreateAssetMenu(fileName = "New Puzzle Set", menuName = "WordPuzzle/Puzzle Set", order = 0)]
+    public class PuzzleSet : ScriptableObject, IPuzzleCollection
+    {
+        [SerializeField]
+        private PuzzlesList _puzzles;
+        
+        public void Add(Puzzle puzzle) => _puzzles.Add(puzzle);
+
+        public void AddRange(IEnumerable<Puzzle> puzzles) => _puzzles.AddRange(puzzles);
+
+        public Option<Puzzle> NextPuzzle() => _puzzles.NextPuzzle();
+    }
+}
