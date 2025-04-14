a47aa984a8e745d050263029c8d754a28a5c5a83
Alexander Komarov <komarov@medvr.pro>
2025-04-14 11:47:22 +0300

    loading screen created

--- Assets/WordPuzzle/Runtime/LoadingScreenController.cs
+++ Assets/WordPuzzle/Runtime/LoadingScreenController.cs
@@ -1,7 +1,34 @@
-﻿namespace WordPuzzle
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WordPuzzle
 {
-    public class LoadingScreen
+    public class LoadingScreenController : MonoBehaviour
     {
+        private static LoadingScreenController _instance;
+        public static LoadingScreenController Instance => _instance;
         
+        [SerializeField]
+        private Image _loadingBar;
+
+        [SerializeField]
+        private TMP_Text _actionTitle;
+
+
+        public void SetProgress(float progress)
+        {
+            _loadingBar.fillAmount = progress;
+        }
+
+        public void SetAction(string action)
+        {
+            _actionTitle.text = action;
+        }
+
+        private void Awake()
+        {
+            _instance = this;
+        }
     }
 }
