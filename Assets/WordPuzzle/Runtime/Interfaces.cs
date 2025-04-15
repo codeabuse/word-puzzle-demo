8723f801c31fbb0232eaf0e868ff9091cb528db9
Alexander Komarov <komarov@medvr.pro>
2025-04-15 14:27:01 +0300

    ILoadProgressHandler interface

--- Assets/WordPuzzle/Runtime/Interfaces.cs
+++ Assets/WordPuzzle/Runtime/Interfaces.cs
@@ -1,8 +1,17 @@
 ﻿using System.Collections.Generic;
 using Codeabuse;
+using Cysharp.Threading.Tasks;
 
 namespace WordPuzzle
 {
+    public interface ILoadProgressHandler
+    {
+        void Show();
+        void SetProgress(float progress, string message = default);
+        void SetMessage(string message);
+        UniTask Hide(float fadeOutSeconds = 0f);
+    }
+    
     public interface IPuzzleManager
     {
         public IEnumerable<int> AvailablePuzzlesWordLength { get; }
