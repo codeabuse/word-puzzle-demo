498f6140d5ae8a518f5dccc9cffe8bb72af854c7
Alexander Komarov <komarov@medvr.pro>
2025-04-15 14:33:31 +0300

    ConfigKeys updated

--- Assets/WordPuzzle/Configuration/Runtime/ConfigKeys.cs
+++ Assets/WordPuzzle/Configuration/Runtime/ConfigKeys.cs
@@ -2,7 +2,6 @@
 {
     public static class ConfigKeys
     {
-        public const string WORD_LENGTH = "game_word_length";
         public const string PUZZLE_DAILY = "puzzle_daily";
         public const string PUZZLE_SETS = "puzzle_sets";
     }
