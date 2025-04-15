8fbace87c7d552008719228042d2049cd60cf756
Alexander Komarov <komarov@medvr.pro>
2025-04-15 14:28:14 +0300

    LoadingScreen prefab, controller updated

--- Assets/WordPuzzle/Runtime/LoadingScreenController.cs
+++ Assets/WordPuzzle/Runtime/LoadingScreenController.cs
@@ -1,34 +1,89 @@
-﻿using TMPro;
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Zenject;
 
 namespace WordPuzzle
 {
-    public class LoadingScreenController : MonoBehaviour
+    public class LoadingScreenController : MonoInstaller, ILoadProgressHandler
     {
-        private static LoadingScreenController _instance;
-        public static LoadingScreenController Instance => _instance;
+        private CanvasGroup _rootCanvas;
         
         [SerializeField]
         private Image _loadingBar;
 
+        [FormerlySerializedAs("_actionTitle")]
         [SerializeField]
-        private TMP_Text _actionTitle;
+        private TMP_Text _message;
+        [SerializeField]
+        private string _defaultMessage = "LOADING...";
 
+        public void Show()
+        {
+            _rootCanvas.enabled = true;
+            _rootCanvas.alpha = 1;
+            _rootCanvas.blocksRaycasts = true;
+        }
 
-        public void SetProgress(float progress)
+        public void SetProgress(float progress, string message = default)
         {
             _loadingBar.fillAmount = progress;
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                _message.text = message;
+            }
         }
 
-        public void SetAction(string action)
+        public void SetMessage(string message)
         {
-            _actionTitle.text = action;
+            _message.text = message;
         }
+        
+        public UniTask Hide(float fadeOutSeconds = 0)
+        {
+            fadeOutSeconds = Mathf.Clamp(fadeOutSeconds, 0, float.PositiveInfinity);
+            if (fadeOutSeconds > 0)
+            { 
+                return _rootCanvas.DOFade(0, fadeOutSeconds)
+                       .OnComplete(SetHide)
+                       .ToUniTask();
+            }
+            else
+            {
+                SetHide();
+            }
+            return UniTask.CompletedTask;
+        }
+
+        private void SetHide()
+        {
+            _rootCanvas.alpha = 0;
+            _rootCanvas.blocksRaycasts = false;
+        }
+        // public override void InstallBindings()
+        // {
+        //     ProjectContext.Instance.Container.QueueForInject(this);
+        // }
 
         private void Awake()
         {
-            _instance = this;
+            _rootCanvas = GetComponentInParent<CanvasGroup>();
+            SetProgress(0);
+            SetMessage(_defaultMessage);
+            Hide();
+        }
+
+        private void OnDestroy()
+        {
+            var container = ProjectContext.Instance.Container;
+            if (container.HasBinding<ILoadProgressHandler>() &&
+                container.Resolve<ILoadProgressHandler>() is {} impl &&  
+                impl.Equals(this))
+                container.Unbind<ILoadProgressHandler>();
         }
     }
 }
