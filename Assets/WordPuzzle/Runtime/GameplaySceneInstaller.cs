a80c355f2ab058ca5f327405b0079f82d486a6a8
Alexander Komarov <komarov@medvr.pro>
2025-04-20 23:26:25 +0300

    gameplay scene and installer

---
+++ Assets/WordPuzzle/Runtime/GameplaySceneInstaller.cs
@@ -0,0 +1,37 @@
+﻿using Codeabuse.Pooling;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Zenject;
+
+namespace WordPuzzle
+{
+    public class GameplaySceneInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private GraphicRaycaster _graphicRaycaster;
+        [SerializeField]
+        private Letter _letterPrefab;
+        [SerializeField]
+        private LetterCell _letterCellPrefab;
+        [FormerlySerializedAs("_lettersPoolRoot")]
+        [SerializeField]
+        private Transform _commonPoolRoot;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<GraphicRaycaster>()
+                   .FromInstance(_graphicRaycaster)
+                   .AsSingle();
+            
+            Container.Bind<PrefabPool<Letter>>()
+                   .FromInstance(new PrefabPool<Letter>(_letterPrefab, _commonPoolRoot))
+                   .AsSingle()
+                   .NonLazy();
+            Container.Bind<PrefabPool<LetterCell>>()
+                   .FromInstance(new PrefabPool<LetterCell>(_letterCellPrefab, _commonPoolRoot))
+                   .AsSingle()
+                   .NonLazy();
+        }
+    }
+}
