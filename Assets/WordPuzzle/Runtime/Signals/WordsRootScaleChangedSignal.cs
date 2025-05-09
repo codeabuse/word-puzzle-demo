3c1ad72499971746f8c51c07f1f9501b85ca190f
Alexander Komarov <komarov@medvr.pro>
2025-05-09 12:30:49 +0300

    signal for game board scale change

---
+++ Assets/WordPuzzle/Runtime/Signals/WordsRootScaleChangedSignal.cs
@@ -0,0 +1,12 @@
+﻿namespace WordPuzzle.Signals
+{
+    public class WordsRootScaleChangedSignal
+    {
+        public float Scale { get; }
+
+        public WordsRootScaleChangedSignal(float scale)
+        {
+            Scale = scale;
+        }
+    }
+}
